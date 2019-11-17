f72ad0d4d37290d0fe4dc4e7d80ed9a64c74fcc3
Colin Chan <colin.th.chan@gmail.com>
2019-11-17 03:54:15 -0500

    Update upstream names and remove create react defaults

--- containers/api/src/main.py
+++ containers/api/src/main.py
@@ -13,4 +13,4 @@ def main():
 
 @bp.route('/test', methods=['GET'])
 def test():
-    return {'data': '< This is an API request... >'}
+    return {'data': 'colin at ctchan.me'}
--- containers/app/src/App.js
+++ containers/app/src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -17,18 +16,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Colin Chan
+        </h1>
         <p>
           {data}
         </p>
